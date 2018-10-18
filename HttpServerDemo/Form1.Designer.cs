9e0ad52d4a90fd4dbe8daaa7e8d17fe86ad30603
cnzi <258345024@qq.com>
2018-10-18 15:26:30 +0800

    first commit

---
+++ HttpServerDemo/Form1.Designer.cs
@@ -0,0 +1,237 @@
+﻿namespace HttpServerDemo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.labelServer = new System.Windows.Forms.Label();
+            this.textBoxIP = new System.Windows.Forms.TextBox();
+            this.textBoxPort = new System.Windows.Forms.TextBox();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.buttonOpenPath = new System.Windows.Forms.Button();
+            this.buttonStop = new System.Windows.Forms.Button();
+            this.labelPath = new System.Windows.Forms.Label();
+            this.textBoxPath = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(0, 0);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(816, 304);
+            this.textBox1.TabIndex = 0;
+            // 
+            // labelServer
+            // 
+            this.labelServer.AutoSize = true;
+            this.labelServer.Location = new System.Drawing.Point(26, 17);
+            this.labelServer.Name = "labelServer";
+            this.labelServer.Size = new System.Drawing.Size(41, 12);
+            this.labelServer.TabIndex = 1;
+            this.labelServer.Text = "server";
+            // 
+            // textBoxIP
+            // 
+            this.textBoxIP.Location = new System.Drawing.Point(85, 12);
+            this.textBoxIP.Name = "textBoxIP";
+            this.textBoxIP.Size = new System.Drawing.Size(237, 21);
+            this.textBoxIP.TabIndex = 2;
+            // 
+            // textBoxPort
+            // 
+            this.textBoxPort.Location = new System.Drawing.Point(337, 11);
+            this.textBoxPort.Name = "textBoxPort";
+            this.textBoxPort.Size = new System.Drawing.Size(40, 21);
+            this.textBoxPort.TabIndex = 4;
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Location = new System.Drawing.Point(462, 12);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(75, 42);
+            this.buttonStart.TabIndex = 5;
+            this.buttonStart.Text = "启动";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3});
+            this.listView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listView1.Location = new System.Drawing.Point(0, 0);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(816, 188);
+            this.listView1.TabIndex = 6;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.ItemMouseHover += new System.Windows.Forms.ListViewItemMouseHoverEventHandler(this.listView1_ItemMouseHover);
+            this.listView1.Click += new System.EventHandler(this.listView1_Click);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "URL";
+            this.columnHeader1.Width = 252;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "文件名";
+            this.columnHeader2.Width = 258;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "返回内容";
+            this.columnHeader3.Width = 200;
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Location = new System.Drawing.Point(-1, 66);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.listView1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.textBox1);
+            this.splitContainer1.Size = new System.Drawing.Size(816, 499);
+            this.splitContainer1.SplitterDistance = 188;
+            this.splitContainer1.TabIndex = 7;
+            // 
+            // buttonOpenPath
+            // 
+            this.buttonOpenPath.Font = new System.Drawing.Font("宋体", 10F);
+            this.buttonOpenPath.Location = new System.Drawing.Point(337, 38);
+            this.buttonOpenPath.Name = "buttonOpenPath";
+            this.buttonOpenPath.Size = new System.Drawing.Size(40, 23);
+            this.buttonOpenPath.TabIndex = 10;
+            this.buttonOpenPath.Text = "...";
+            this.buttonOpenPath.UseVisualStyleBackColor = true;
+            // 
+            // buttonStop
+            // 
+            this.buttonStop.Location = new System.Drawing.Point(572, 12);
+            this.buttonStop.Name = "buttonStop";
+            this.buttonStop.Size = new System.Drawing.Size(75, 42);
+            this.buttonStop.TabIndex = 9;
+            this.buttonStop.Text = "停止";
+            this.buttonStop.UseVisualStyleBackColor = true;
+            this.buttonStop.Click += new System.EventHandler(this.buttonStop_Click);
+            // 
+            // labelPath
+            // 
+            this.labelPath.AutoSize = true;
+            this.labelPath.Location = new System.Drawing.Point(26, 42);
+            this.labelPath.Name = "labelPath";
+            this.labelPath.Size = new System.Drawing.Size(53, 12);
+            this.labelPath.TabIndex = 8;
+            this.labelPath.Text = "文件路径";
+            // 
+            // textBoxPath
+            // 
+            this.textBoxPath.Location = new System.Drawing.Point(85, 39);
+            this.textBoxPath.Name = "textBoxPath";
+            this.textBoxPath.Size = new System.Drawing.Size(237, 21);
+            this.textBoxPath.TabIndex = 7;
+            this.textBoxPath.TextChanged += new System.EventHandler(this.textBoxPath_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(672, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 12);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "serverStatus";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(815, 562);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonOpenPath);
+            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.buttonStop);
+            this.Controls.Add(this.textBoxIP);
+            this.Controls.Add(this.labelPath);
+            this.Controls.Add(this.textBoxPath);
+            this.Controls.Add(this.textBoxPort);
+            this.Controls.Add(this.buttonStart);
+            this.Controls.Add(this.labelServer);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Click += new System.EventHandler(this.Form1_Click);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label labelServer;
+        private System.Windows.Forms.TextBox textBoxIP;
+        private System.Windows.Forms.TextBox textBoxPort;
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Button buttonStop;
+        private System.Windows.Forms.Label labelPath;
+        private System.Windows.Forms.TextBox textBoxPath;
+        private System.Windows.Forms.Button buttonOpenPath;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ HttpServerDemo/Form1.cs
@@ -0,0 +1,260 @@
+﻿using System;
+using System.Collections;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Windows.Forms;
+using System.Drawing;
+using System.Collections.Generic;
+
+namespace HttpServerDemo
+{
+
+    public partial class Form1 : Form
+    {
+        #region HttpServer相关变量
+        public string Ip = Dns.GetHostEntry(Dns.GetHostName())
+            .AddressList.FirstOrDefault<IPAddress>(a => a.AddressFamily.ToString().Equals("InterNetwork")).ToString();
+
+        public int Port = 9091;
+
+        public string EnvironmentUserName = Environment.UserName;
+
+        private THttpListener _httpListener;
+        #endregion
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            this.WindowState = FormWindowState.Normal;
+            this.StartPosition = FormStartPosition.CenterScreen;
+            this.FormBorderStyle = FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+
+            label1.Text = "";
+            textBoxIP.Text = Ip;
+            textBoxPort.Text = Port.ToString();
+
+            textBoxPath.Text = Application.StartupPath;
+        }
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            if (textBoxIP.Text == "" || textBoxPort.Text == "" || listView1.Items.Count == 0)
+                return;
+
+            textBoxPath_TextChanged(sender, e);
+
+            buttonStart.Enabled = false;
+            buttonStop.Enabled = true;
+            label1.Text = "HttpServer已启动";
+            label1.ForeColor = Color.FromArgb(0, 0, 255);
+
+            Port = int.Parse(textBoxPort.Text.Trim());
+            //添加防火墙例外端口，供客户端访问
+            INetFwManger.NetFwAddPorts("ODOO ", Port, "TCP");
+
+            #region 执行dos命令
+            DosCommandOperation dosCommandOperation = new DosCommandOperation();
+            string dosRet = dosCommandOperation.Execute(string.Format("netsh http add urlacl url=http://{0}:{1}/ user={2}", Ip, Port, EnvironmentUserName));
+            #endregion
+
+            #region 启动HttpServer
+            List<string> listUrl = new List<string>();
+            dicUrlResponse.Clear();
+            foreach (ListViewItem item in listView1.Items)
+            {
+                listUrl.Add(item.SubItems[0].Text);
+                dicUrlResponse.Add(item.SubItems[0].Text, item.SubItems[2].Text);
+            }
+            _httpListener = new THttpListener(listUrl.ToArray());
+
+            _httpListener.ResponseEvent += _HttpListener_ResponseEvent;
+            _httpListener.Start();
+            #endregion
+        }
+        Dictionary<string, string> dicUrlResponse = new Dictionary<string, string>();
+
+        private void buttonStop_Click(object sender, EventArgs e)
+        {
+            if (_httpListener != null)
+            {
+                buttonStart.Enabled = true;
+                buttonStop.Enabled = false;
+                label1.Text = "HttpServer已停止";
+                label1.ForeColor = Color.Red;
+
+                _httpListener.Stop();
+            }
+        }
+
+        #region HttpServer相关
+
+        void _HttpListener_ResponseEvent(System.Net.HttpListenerContext ctx)
+        {
+            //GET 还是 POST 是根据访问方式得来的           
+            try
+            {
+                //响应消息 根据第一列，响应第三列
+                Invoke((MethodInvoker)(() =>
+                {
+                    foreach (ListViewItem item in listView1.Items)
+                    {
+                        string url = ctx.Request.Url.ToString();
+                        url = url.Contains('?') ? url.Substring(0, url.IndexOf('?')) : url;
+
+                        if (item.SubItems[0].Text == url || item.SubItems[0].Text == url + "/")
+                        {
+                            ResponseWrite(item.SubItems[2].Text, ctx.Response);
+                        }
+
+                    }
+                }));
+
+            }
+            catch (Exception ex)
+            {
+            }
+
+            //测试页面
+            //if (!ctx.Request.Url.AbsolutePath.ToLower().Contains("login"))
+            //{
+            //    ResponseWrite("<html><head><title>ODOO</title></head><body><div><h1>这是一个测试页面</h1><p>如果能打开这个页面，那么HttpServer已经启动成功了。</body></html>", ctx.Response, "text/html");
+            //}
+            //else
+            //{
+            //    ResponseWrite("ok", ctx.Response);
+            //}
+        }
+
+        /// <summary>
+        /// http响应
+        /// </summary>
+        /// <param name="msg"></param>
+        /// <param name="response"></param>
+        /// <param name="type"></param>
+        public void ResponseWrite(string msg, System.Net.HttpListenerResponse response, string type = "text/plain")
+        {
+            try
+            {
+                //使用Writer输出http响应代码
+                if (type == "text/plain")
+                {
+                    //using (System.IO.StreamWriter writer = new System.IO.StreamWriter(response.OutputStream, new UTF8Encoding()))
+                    //{
+                    //    response.ContentType = type + ";charset=utf-8";
+                    //    writer.WriteLine(msg);
+                    //    writer.Close();
+                    //    response.Close();
+                    //}
+                }
+
+                using (StreamWriter writer = new StreamWriter(response.OutputStream))
+                {
+                    response.StatusCode = 200;
+                    response.Headers.Add("Access-Control-Allow-Origin", "*");
+                    response.ContentType = "application/json";
+                    response.ContentEncoding = Encoding.UTF8;
+
+                    writer.WriteLine(msg);
+                    writer.Close();
+                    response.Close();
+                }
+
+            }
+            catch (Exception exception)
+            {
+            }
+        }
+
+        #endregion
+
+        #region 其他控件事件
+        static List<FileInformation> FileList = new List<FileInformation>();
+        public static List<FileInformation> GetAllFiles(DirectoryInfo dir)
+        {
+            FileInfo[] allFile = dir.GetFiles();
+            foreach (FileInfo fi in allFile)
+            {
+                FileList.Add(new FileInformation { FileName = fi.Name, FilePath = fi.FullName, FileRout = fi.DirectoryName });
+            }
+            DirectoryInfo[] allDir = dir.GetDirectories();
+            foreach (DirectoryInfo d in allDir)
+            {
+                GetAllFiles(d);
+            }
+            return FileList;
+        }
+
+        public class FileInformation
+        {
+            public string FileName { get; set; }
+            public string FilePath { get; set; }
+            public string FileRout { get; set; }
+        }
+
+        private void textBoxPath_TextChanged(object sender, EventArgs e)
+        {
+            FileList.Clear();
+            listView1.Items.Clear();
+            string path = textBoxPath.Text;
+            if (Directory.Exists(path))
+            {
+                List<FileInformation> list = GetAllFiles(new DirectoryInfo(path));
+
+                //去掉重复的  只保留第一个                      
+                list = list.Where((x, i) => list.FindIndex(z => z.FileRout == x.FileRout) == i).ToList();
+
+                foreach (var item in list)
+                {
+                    Console.WriteLine(string.Format("文件名：{0}---文件目录{1}", item.FileName, item.FilePath));
+                    string url = "http://" + item.FileRout.Replace(path, textBoxIP.Text.Trim() + ":" + textBoxPort.Text.Trim()).Replace('\\', '/') + "/";
+                    string content = new StreamReader(item.FilePath, Encoding.Default).ReadToEnd();
+                    listView1.Items.Add(new ListViewItem(new string[] { url, item.FileName, content }));
+                }
+            }
+        }
+
+        private void listView1_ItemMouseHover(object sender, ListViewItemMouseHoverEventArgs e)
+        {
+            ToolTip toolTip = new ToolTip();
+
+            string itemInfor =
+            "URL" + e.Item.SubItems[0].Text + "\n" +
+            "文件名：" + e.Item.SubItems[1].Text + "\n" +
+            "内容：" + e.Item.SubItems[2].Text + "\n";
+
+            toolTip.SetToolTip((e.Item).ListView, itemInfor);
+        }
+
+        private void listView1_Click(object sender, EventArgs e)
+        {
+            //没选中，或没启动监听
+            if (listView1.SelectedItems.Count == 0 || buttonStart.Enabled == true)
+                return;
+
+            //获取选中行 第1列的内容
+            string text = listView1.SelectedItems[0].SubItems[0].Text; 
+            System.Diagnostics.Process.Start("explorer.exe", text);
+        }
+
+        bool isCollapsed = false;
+        private void Form1_Click(object sender, EventArgs e)
+        {
+            if (!isCollapsed)
+            {
+                splitContainer1.Panel1Collapsed = true;
+                isCollapsed = true;
+            }
+            else
+            {
+                splitContainer1.Panel1Collapsed = false;
+                isCollapsed = false;
+            }
+        }
+        #endregion
+
+    }
+}
